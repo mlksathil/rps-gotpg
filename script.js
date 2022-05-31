911a39ebd9428f7d54dae7db6ea21002da21a40a
mlksathil <mahmoodathil12@gmail.com>
2022-05-31 14:27:00 +0530

    Update script:
    - Keep seaparate eventlisteners not inside a function
    - Avoid using 'currentTarget'
    - Add gif
    
    Update HTML:
    - Add div for gif
    
    Update CSS:
    - Update Hover on start/stop buttons

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ let result = document.querySelector('#result');
 let condition = document.querySelector('#condition');
 let playerScoreBoard = document.querySelector('#pboard');
 let computerScoreBoard = document.querySelector('#cboard');
+let sr_c = document.querySelector('.sr_container');
+let image;
+let button = document.querySelector("body > button")
+
+let computercount = playercount = play = 0;
+let gameend = false;
+let gifshow = 0;
 
 // SECTION: Condition Checking
 function conditionPlay(computerSelection, playerSelection) {
@@ -53,101 +60,150 @@ function roundStart(choice) {
 
 }
 
-// SECTION: Game Start Function
-function gameStart(e) {
-    const countPlay = 5;
-    let computercount = 0;
-    let playercount = 0;
-    let play = 0;
 
+function sr() {
 
-    // Make Conatiner Visible
-    container.style.setProperty('display','flex');
+    if(gifshow == 0) {
 
-    // Change Button Status
-    let button = e.target
-    button.textContent = "Run from Game";
-    button.classList.add('stop');
+        image = document.createElement('img');
+
+        gifshow++;
+
+        container.style.setProperty('display','none');
+        sr_c.style.setProperty('display', 'block');
+
+        if (button.textContent == "Bargain??!")
+            image.setAttribute('src','./images/gif/bargain.gif')
+        else if (button.textContent == "Run from Game!")
+            image.setAttribute('src','./images/gif/run.gif')
+        else if (button.textContent == "Show Off!!!")
+            image.setAttribute('src','./images/gif/showoff.gif')
+
+        sr_c.appendChild(image);
+    }
+
+}
+
+function gameRestart() {
+            
+    // SECTION: Checks for Game End
+    if (gameend) {
+
+        image.remove();
+        
+        console.log("Enter Game End");
+        
+        if (button.textContent == "Run from Game!" || button.textContent == "Bargain??!") {
+            button.textContent = "Bargain? Please!";
+            clanStatus.textContent = 'Save your Clan!';
+            button.classList.remove('stop');
+        } else {
+            button.textContent = "Play Rematch!";
+            clanStatus.textContent = 'Mock them with a Rematch!';
+        }
+        
+        clanStatus.style.removeProperty('color');
+        sr_c.style.setProperty('display', 'none');
+
+        // SECTION: Reset Everything
+        computercount = 0;
+        playercount = 0;
+        play = 0;
+        gameend = false;
+        gifshow = 0;
+        
+        computerText.textContent = '';
+        playerText.textContent = '';
+        
+        roundStatus.textContent = '';
+        roundStatus.style.removeProperty('color');
+        result.textContent = '';
+        result.style.removeProperty('color');
+        
+        condition.textContent = '';
+
+        playerScoreBoard.children[1].textContent = '0';
+        computerScoreBoard.children[1].textContent = '0';
+    }
+}
+
+// SECTION: rps_Buttons Click
+let imgButtons = document.querySelectorAll('.b2')
+imgButtons.forEach(b => b.addEventListener('click', function(e) {
+
+    if (gameend) return  sr(), setTimeout(gameRestart, 5000) ;
+
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
 
     let tmpWin = 0;
+    const countPlay = 5;
 
-    let imgButtons = document.querySelectorAll('.b2')
-    imgButtons.forEach(b => b.addEventListener('click', function(e) {
+    choice = e.target.dataset.bt;
 
-        choice = e.currentTarget.dataset.bt;
+    play++;
+    roundStatus.textContent = `Round ${play}`;
 
-        play++;
-        roundStatus.textContent = `Round ${play}`;
+    // Check Status of one round
+    tmpWin = roundStart(choice);
+    
+    if (tmpWin === "Computer") {
+        computercount++;
+    }
+    else if (tmpWin == "Tie");
+    else {
+        playercount++;
+    }
 
-        // Check Status of one round
-        tmpWin = roundStart(choice);
-        
-        if (tmpWin === "Computer") {
-            computercount++;
+    playerScoreBoard.children[1].textContent = `${playercount}`;
+    computerScoreBoard.children[1].textContent = `${computercount}`;
+
+    if (computercount >= countPlay || playercount >= countPlay) {
+        // console.log('Enter Before Game End')
+        roundStatus.textContent = 'Game Over';
+        gameend = true;
+
+        if (playercount > computercount) {
+            result.textContent = `You Won the Match`;
+            result.style.setProperty('color','green');
+            clanStatus.textContent = "You saved your Clan!";
+            clanStatus.style.setProperty('color','green');
+            button.textContent = "Show Off!!!"
+            button.classList.remove('stop')
+        } else {
+            result.textContent = `Your Opponent Won the Match`;
+            result.style.setProperty('color','red');
+            clanStatus.textContent = 'Your Clan is in Despair!';
+            clanStatus.style.setProperty('color','red');
+            button.textContent = "Bargain??!"
         }
-        else if (tmpWin == "Tie");
-        else {
-            playercount++;
-        }
-
-        playerScoreBoard.children[1].textContent = `${playercount}`;
-        computerScoreBoard.children[1].textContent = `${computercount}`;
-
-        if (play >= countPlay) {
-            console.log('Enter Before Game End')
-            roundStatus.textContent = 'Game Over';
-            if (playercount > computercount) {
-                result.textContent = `You Won the Match by ${playercount}`;
-                result.style.setProperty('color','green');
-                clanStatus.innerHTML = "<span style='color:green;'>You saved your Clan!</span>";
-            } else if (playercount === computercount) {
-                result.textContent = "It's a Tie - Replay the Match!";
-                result.style.setProperty('color','black');
-            } else {
-                result.textContent = `Your Opponent Won the Match by ${computercount}`;
-                result.style.setProperty('color','red');
-                clanStatus.innerHTML = '<span style="color:red;">Your Clan is in Despair!</span>';
-            }
-        }   
-
-        // SECTION: Checks for Game End
-        if (play > countPlay) {
-            console.log("Enter Game End");
-            button.textContent = "Bargain for Game!";
-            button.classList.remove('stop');
-            container.style.setProperty('display','none');
-
-            // SECTION: Reset Everything
-            computercount = 0;
-            playercount = 0;
-            play = 0;
-
-            clanStatus.textContent = 'Save your Clan!';
-            
-            computerText.textContent = '';
-            playerText.textContent = '';
-            
-            roundStatus.textContent = '';
-            roundStatus.style.removeProperty('color');
-            result.textContent = '';
-            result.style.removeProperty('color');
-            
-            condition.textContent = '';
-
-            playerScoreBoard.children[1].textContent = '0';
-            computerScoreBoard.children[1].textContent = '0';
-        }
-    },false)
-    );
-}
+    }   
+},false));
 
 // SECTION: Starting the game
 let start = document.querySelector('.start');
 start.addEventListener('click', e => {
-    if (e.target.textContent === 'Start the Game!' || e.target.textContent === 'Bargain for Game!') gameStart(e)
-    else {
-        e.target.textContent = "Start the Game!";
+    if (button.textContent === 'Start the Game!') {
+
+        // Make Conatiner Visible
+        container.style.setProperty('display','flex');
+
+        // Change Button Status
+        button.textContent = "Run from Game!";
+        button.classList.add('stop');
+    
+    } else if (gifshow > 0) {
+        gameRestart();
+    } else if (button.textContent === 'Bargain??!' || button.textContent === 'Show Off!!!' || button.textContent === "Run from Game!") { 
+        sr();
+        gameend = true;
+        setTimeout(gameRestart, 5000);
+    } else {
+        gameRestart()
+        
+        button.textContent = "Start the Game!";
         container.style.setProperty('display','none');
-        e.target.classList.remove('stop');
+        button.classList.remove('stop');
+        clanStatus.textContent = 'Save your Clan!'
+        clanStatus.style.setProperty('color','black');
     }
 });
